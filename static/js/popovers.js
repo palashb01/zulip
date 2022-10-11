6b881e88dbd7bf27c6c4c994f979c22a0c4d64e1
Palash Baderia <palash.baderia@outlook.com>
2022-10-11 17:50:27 +0530

    message_feed: Add Button `quote and reply` to the message bar.
    Add a `Quote and reply or forward` button in the message_header_bar
    and removed the same option from the three-dot menu.
    Fixes #23100.

--- static/js/popovers.js
+++ static/js/popovers.js
@@ -504,6 +504,171 @@ function show_user_group_info_popover(element, group, message) {
     }
 }
 
+export function toggle_actions_popover(element, id) {
+    const $last_popover_elem = $current_actions_popover_elem;
+    hide_all();
+    if ($last_popover_elem !== undefined && $last_popover_elem.get()[0] === element) {
+        // We want it to be the case that a user can dismiss a popover
+        // by clicking on the same element that caused the popover.
+        return;
+    }
+
+    $(element).closest(".message_row").toggleClass("has_popover has_actions_popover");
+    message_lists.current.select_id(id);
+    const not_spectator = !page_params.is_spectator;
+    const $elt = $(element);
+    if ($elt.data("popover") === undefined) {
+        const message = message_lists.current.get(id);
+        const message_container = message_lists.current.view.message_containers.get(message.id);
+        const should_display_hide_option =
+            muted_users.is_user_muted(message.sender_id) &&
+            !message_container.is_hidden &&
+            not_spectator;
+        const editability = message_edit.get_editability(message);
+        const can_move_message = message_edit.can_move_message(message);
+
+        let editability_menu_item;
+        let move_message_menu_item;
+        let view_source_menu_item;
+
+        if (editability === message_edit.editability_types.FULL) {
+            editability_menu_item = $t({defaultMessage: "Edit message"});
+            if (message.is_stream) {
+                move_message_menu_item = $t({defaultMessage: "Move messages"});
+            }
+        } else if (can_move_message) {
+            move_message_menu_item = $t({defaultMessage: "Move messages"});
+            view_source_menu_item = $t({defaultMessage: "View message source"});
+        } else {
+            view_source_menu_item = $t({defaultMessage: "View message source"});
+        }
+
+        // We do not offer "Mark as unread" on messages in streams
+        // that the user is not currently subscribed to. Zulip has an
+        // invariant that all unread messages must be in streams the
+        // user is subscribed to, and so the server will ignore any
+        // messages in such streams; it's better to hint this is not
+        // useful by not offering the option.
+        //
+        // We also require that the message is currently marked as
+        // read. Theoretically, it could be useful to offer this even
+        // for a message that is already unread, so you can mark those
+        // below it as unread; but that's an unlikely situation, and
+        // showing it can be a confusing source of clutter. We may
+        // want to revise this algorithm specifically in the context
+        // of interleaved views.
+        //
+        // To work around #22893, we also only offer the option if the
+        // fetch_status data structure means we'll be able to mark
+        // everything below the current message as read correctly.
+        const not_stream_message = message.type !== "stream";
+        const subscribed_to_stream =
+            message.type === "stream" && stream_data.is_subscribed(message.stream_id);
+        const should_display_mark_as_unread =
+            !message.unread && not_spectator && (not_stream_message || subscribed_to_stream);
+
+        const should_display_edit_history_option =
+            message.edit_history &&
+            message.edit_history.some(
+                (entry) =>
+                    entry.prev_content !== undefined ||
+                    entry.prev_stream !== undefined ||
+                    entry.prev_topic !== undefined,
+            ) &&
+            page_params.realm_allow_edit_history &&
+            not_spectator;
+
+        // Disabling this for /me messages is a temporary workaround
+        // for the fact that we don't have a styling for how that
+        // should look.  See also condense.js.
+        const should_display_collapse =
+            !message.locally_echoed &&
+            !message.is_me_message &&
+            !message.collapsed &&
+            not_spectator;
+        const should_display_uncollapse =
+            !message.locally_echoed && !message.is_me_message && message.collapsed;
+
+        const conversation_time_uri = hash_util.by_conversation_and_time_url(message);
+
+        const should_display_delete_option =
+            message_edit.get_deletability(message) && not_spectator;
+        const should_display_read_receipts_option =
+            page_params.realm_enable_read_receipts && not_spectator;
+
+        const args = {
+            message_id: message.id,
+            stream_id: message.stream_id,
+            editability_menu_item,
+            move_message_menu_item,
+            should_display_mark_as_unread,
+            view_source_menu_item,
+            should_display_collapse,
+            should_display_uncollapse,
+            should_display_add_reaction_option: message.sent_by_me,
+            should_display_edit_history_option,
+            should_display_hide_option,
+            conversation_time_uri,
+            narrowed: narrow_state.active(),
+            should_display_delete_option,
+            should_display_read_receipts_option,
+            should_display_reminder_option: feature_flags.reminders_in_message_action_menu,
+        };
+
+        const ypos = $elt.offset().top;
+        $elt.popover({
+            // Popover height with 7 items in it is ~190 px
+            placement: message_viewport.height() - ypos < 220 ? "top" : "bottom",
+            title: "",
+            content: render_actions_popover_content(args),
+            template: render_actions_popover_template(),
+            html: true,
+            trigger: "manual",
+        });
+        $elt.popover("show");
+        $current_actions_popover_elem = $elt;
+    }
+
+    if (window.innerWidth < media_breakpoints_num.xl) {
+        // This ensures that the popover doesn't overflow to the right of the window.
+        const actions_popover_left = window.innerWidth - $(".actions_popover_wrapper").outerWidth();
+        $(".actions_popover_wrapper").css("left", actions_popover_left);
+    }
+}
+
+export function render_actions_remind_popover(element, id) {
+    hide_all();
+    $(element).closest(".message_row").toggleClass("has_popover has_actions_popover");
+    message_lists.current.select_id(id);
+    const $elt = $(element);
+    if ($elt.data("popover") === undefined) {
+        const message = message_lists.current.get(id);
+        const args = {
+            message,
+        };
+        const ypos = $elt.offset().top;
+        $elt.popover({
+            // Popover height with 7 items in it is ~190 px
+            placement: message_viewport.height() - ypos < 220 ? "top" : "bottom",
+            title: "",
+            content: render_remind_me_popover_content(args),
+            html: true,
+            trigger: "manual",
+        });
+        $elt.popover("show");
+        current_flatpickr_instance = $(
+            `.remind.custom[data-message-id="${CSS.escape(message.id)}"]`,
+        ).flatpickr({
+            enableTime: true,
+            clickOpens: false,
+            defaultDate: "today",
+            minDate: "today",
+            plugins: [new ConfirmDatePlugin({})],
+        });
+        $current_actions_popover_elem = $elt;
+    }
+}
+
 function get_action_menu_menu_items() {
     const $current_actions_popover_elem = $("[data-tippy-root] .actions_popover");
     if (!$current_actions_popover_elem) {
@@ -990,6 +1155,23 @@ export function register_click_handlers() {
 
         current_user_sidebar_user_id = user.user_id;
         current_user_sidebar_popover = $target.data("popover");
+    });
+
+    $("body").on("click", ".mark_as_unread", (e) => {
+        hide_actions_popover();
+        const message_id = $(e.currentTarget).data("message-id");
+
+        unread_ops.mark_as_unread_from_here(message_id);
+
+        e.stopPropagation();
+        e.preventDefault();
+    });
+
+    $("body").on("click", ".reminder_button", (e) => {
+        const message_id = $(e.currentTarget).data("message-id");
+        render_actions_remind_popover($(".selected_message .actions_hover")[0], message_id);
+        e.stopPropagation();
+        e.preventDefault();
     });
 
     $("body").on("click", ".remind.custom", (e) => {
